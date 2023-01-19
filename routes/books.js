f317512f7a0baacfdfa0824b8de386bd85b42bb3
Vasily Nosikov <vnosikov@gmail.com>
2023-01-19 21:23:10 +0300

    use json in response

--- routes/books.js
+++ routes/books.js
@@ -4,6 +4,6 @@ const Book = mongoose.model('Book');
 module.exports = app => {
   app.get('/api/books/testcount', async (req, res) => {
     const books = await Book.find();
-    res.send(`${books.length}`);
+    res.send({ count: books.length });
   });
 }
