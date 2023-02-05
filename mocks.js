0c080a5f6a5d31e2d8e8e4427708184acfae28b8
Vasily Nosikov <vnosikov@gmail.com>
2023-02-06 00:13:32 +0300

    nodemon and better mocks

--- mocks.js
+++ mocks.js
@@ -1,22 +1,83 @@
+const B1 = {
+  id: 'b1',
+  authors: [{
+    ru: 'Джеймс Скотт',
+    en: 'James Scott',
+  }],
+  title: {
+    ru: 'Глазами государства',
+    en: 'Seeing like a state',
+  },
+  mainLang: 'en',
+  refsNumber: 2,
+  read: true,
+  marked: true,
+};
+const B2 = {
+  id: 'b2',
+  authors: [{
+    ru: null,
+    en: 'Theodore M. Porter',
+  }],
+  title: {
+    ru: null,
+    en: 'Trust in Numbers',
+  },
+  mainLang: 'en',
+  refsNumber: 1,
+  read: false,
+  marked: false,
+};
+const B3 = {
+  id: 'b3',
+  authors: [{
+    ru: 'Владимир Ленин',
+    en: 'Vladimir Lenin',
+  }],
+  title: {
+    ru: 'Государство и революция',
+    en: 'The State and Revolution',
+  },
+  mainLang: 'ru',
+  refsNumber: 3,
+  read: true,
+  marked: false,
+};
+const B4 = {
+  id: 'b4',
+  authors: [{
+    ru: 'Дмитрий Травин',
+    en: null,
+  }],
+  title: {
+    ru: 'Почему Россия отстала',
+    en: null,
+  },
+  mainLang: 'ru',
+  refsNumber: 0,
+  read: false,
+  marked: false,
+};
+
 const mocks = {
   Query: () => ({
-    books: () => [...new Array(6)],
+    books: [B1, B2, B3, B4],
   }),
-  Book: () => ({
-    id: () => 'just an id',
-    authors: () => [{
-      ru: 'Джеймс Скотт',
-      en: 'James Scott',
-    }],
-    title: () => ({
-      ru: 'Глазами государства',
-      en: 'Seeing like a state',
-    }),
-    mainLang: () => 'en',
-    refsNumber: () => 2,
-    read: () => true,
-    marked: () => true,
-  }),
+  // Book: () => ({
+  //   id: () => 'just an id',
+  //   authors: () => [{
+  //     ru: 'Джеймс Скотт',
+  //     en: 'James Scott',
+  //   }],
+  //   title: () => ({
+  //     ru: 'Глазами государства',
+  //     en: 'Seeing like a state',
+  //   }),
+  //   mainLang: () => 'en',
+  //   refsNumber: () => 2,
+  //   read: () => true,
+  //   marked: () => true,
+  // }),
 };
 
 module.exports = mocks;
